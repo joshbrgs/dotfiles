050f7527b1255c7ca1a117f8d5e61b1fab1076d5
Josh Burgess <burgessj247@gmail.com>
2024-03-26 14:45:58 -0400

    changes nvim dashboard

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -9,4 +9,43 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
+  {
+    "nvimdev/dashboard-nvim",
+    opts = {
+      theme = "hyper",
+      config = {
+        week_header = {
+          enable = true,
+        },
+        packages = { enable = false },
+        project = { enable = false },
+        mru = { limit = 10, icon = "󰢚 ", label = "Recent Files", cwd_only = false },
+        footer = {}, -- footer
+        shortcut = {
+          { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+          { desc = "󰄛 Lazy Extras", group = "@property", action = "LazyExtras", key = "x" },
+          {
+            icon = " ",
+            icon_hl = "@variable",
+            desc = "Files",
+            group = "Label",
+            action = "Telescope find_files",
+            key = "f",
+          },
+          {
+            desc = " Find Word",
+            group = "DiagnosticHint",
+            action = "Telescope live_grep",
+            key = "w",
+          },
+          {
+            desc = " dotfiles",
+            group = "Number",
+            action = "e ~/dotfiles/",
+            key = "d",
+          },
+        },
+      },
+    },
+  },
 }
