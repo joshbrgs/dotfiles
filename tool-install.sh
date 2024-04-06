fd267d0df9cb0a6b02c23549b5b1dd291b27647d
Josh Burgess <burgessj247@gmail.com>
2024-04-06 14:53:38 -0400

    organizes the brew from the pac

--- tool-install.sh
+++ tool-install.sh
@@ -47,7 +47,7 @@ elif [ "$PACKAGE_MANAGER" = "pacman" ]; then
 elif [ "$PACKAGE_MANAGER" = "brew" ]; then
 	# Install using brew
 	INSTALL="brew install "
-  TOOLS="stow go github-cli zsh tmux lolcat lsd neovim powerline k9s ripgrep thefuck fzf neofetch htop bashtop jq "
+  TOOLS="stow go rust deno terraform krew minikube gh protobuf zsh tmux lolcat lsd neovim powerline k9s ripgrep thefuck fzf neofetch htop bashtop jq "
 else
 	echo "Unknown package manager: $PACKAGE_MANAGER":while [[ condition ]]; do
 	 
@@ -59,23 +59,33 @@ function install_tools() {
 }
 
 function curl_tools() {
+  if [ "$PACKAGE_MANAGER" != "brew" ]; then 
+    TER_VER=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1') && /
+    wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_amd64.zip && /
+    unzip terraform_${TER_VER}_linux_amd64.zip && /
+    sudo mv terraform /usr/local/bin/ && /
+
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && /
+    chmod 700 get_helm.sh && /
+    ./get_helm.sh && /
+    curl -fsSL https://deno.land/install.sh | sh && /
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash && /
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" &&
+      unzip awscliv2.zip && /
+    sudo ./aws/install && /
+    curl -fL https://install-cli.jfrog.io | sh && /
+ elif [ "$PACKAGE_MANAGER" = "brew" ]; then 
+   curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg" && /
+   sudo installer -pkg AWSCLIV2.pkg -target / && /
+
+   curl "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-471.0.0-darwin-arm.tar.gz" && /
+   tar -xvf google-cloud-cli-471.0.0-darwin-arm.tar.gz && /
+   ./google-cloud-sdk/install.sh && /
+ fi
   curl -sS https://starship.rs/install.sh | sh && /
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && /
-  curl -fsSL https://deno.land/install.sh | sh && /
-  curl -fL https://install-cli.jfrog.io | sh && /
-  curl -fsSL https://ollama.com/install.sh | sh && /
-  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash && /
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" &&
-    unzip awscliv2.zip && /
-  sudo ./aws/install && /
-  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && /
-  chmod 700 get_helm.sh && /
-  ./get_helm.sh && /
-  TER_VER=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1') && /
-  wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_amd64.zip && /
-  unzip terraform_${TER_VER}_linux_amd64.zip && /
-  sudo mv terraform /usr/local/bin/
-}
+  curl -fsSL https://ollama.com/install.sh | sh
+ }
 
 function git_things() {
    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && /
@@ -83,7 +93,7 @@ function git_things() {
 
 function make_dirs() {
   mkdir Projects bin Work 
-  if [ "$PACKAGE_MANAGER" != "brew" ]; then
+  if [ "$PACKAGE_MANAGER" != "brew" ]; theni
     mkdri Downloads Documents
   fi
 }
@@ -111,6 +121,12 @@ function get_krewed() {
 
 }
 
+function krew_install() {
+  kubectl krew install ctx && /
+  kubectl krew install ns
+
+}
+
 make_dirs
 
 if [ "$PACKAGE_MANAGER" = "pacman" ]; then
@@ -125,13 +141,16 @@ if [ "$PACKAGE_MANAGER" = "pacman" ]; then
   makepkg -si && /
   rm -rf ~/yay && /
   cd ~
+  yay_things
 fi
 
 install_tools
-yay_things
 curl_tools
 git_things
-get_krewed
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then 
+  get_krewed
+fi
+krew_install
 stow_it
 
 echo -e "Thank you for installing \xF0\x9F\x92\xA9 you may still need global npm tools; and please run exec zsh" | lolcat
