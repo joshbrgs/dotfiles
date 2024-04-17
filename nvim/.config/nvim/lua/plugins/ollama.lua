50affb1259ed013b5eb02330de1d90f4e25d95f8
Josh Burgess <burgessj247@gmail.com>
2024-04-17 08:11:18 -0400

    Changes model to gemma

--- nvim/.config/nvim/lua/plugins/ollama.lua
+++ nvim/.config/nvim/lua/plugins/ollama.lua
@@ -4,7 +4,7 @@ return {
   {
     "David-Kunz/gen.nvim",
     opts = {
-      model = "codellama", -- The default model to use.
+      model = "codegemma", -- The default model to use.
       host = "localhost", -- The host running the Ollama service.
       port = "11434", -- The port on which the Ollama service is listening.
       quit_map = "q", -- set keymap for close the response window
@@ -29,7 +29,7 @@ return {
       display_mode = "split", -- The display mode. Can be "float" or "split".
 
       show_prompt = false, -- Shows the prompt submitted to Ollama.
-      show_model = false, -- Displays which model you are using at the beginning of your chat session.
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
       no_auto_close = false, -- Never closes the window automatically.
       debug = false, -- Prints errors and the command which is run.
     },
